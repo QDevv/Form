9701780f4608c4491bfda91a95d3c911125eb772
kudus Lawal <abdulkudus333@gmail.com>
2025-03-23 18:17:49 +0100

    first commit

---
+++ contact-form-main/form.js
@@ -0,0 +1,60 @@
+const redBorder = document.querySelectorAll(".red-border");
+const submit = document.querySelector("button");
+const radio = document.querySelectorAll('input[name="query-type"]');
+const QueryErrrorMessage = document.querySelector(
+  ".queryType-container p:nth-child(3)"
+);
+const textAreaErrorMessage = document.querySelector(".message-container p");
+
+const textArea = document.querySelector("textarea");
+const consentCheck = document.getElementById("consent");
+const consentCheckErrorMessage = document.querySelector(".consent-container p");
+
+const paragraphs = document.querySelector("p");
+
+console.log(paragraphs.parentElement.querySelector("input"));
+
+console.log(radio);
+
+console.log(Array.from(radio)[0]);
+
+submit.addEventListener("click", function () {
+  let inputCheck = Array.from(paragraphs).filter((p) => {
+    return p.parentElement.querySelectorAll("input").value === "";
+  });
+  console.log(inputCheck);
+
+  Array.from(redBorder)
+    .filter((input) => {
+      return input.value === "";
+    })
+    .map((input) => {
+      return (input.style.border = "1px solid red");
+    });
+  //   if (radio.checked) {
+  //     console.log(`Selected value: ${radio.value}`);
+  //   } else {
+  //
+  //   }
+  let checkedRadio = Array.from(radio).filter((query) => {
+    return query.checked;
+  });
+  const [a] = checkedRadio;
+  console.log(a);
+  if (a) {
+    console.log(`Selected value: ${a.value}`);
+  } else {
+    QueryErrrorMessage.style.color = "red";
+  }
+  if (textArea.value) {
+    console.log(`${textArea.value}`);
+  } else {
+    textAreaErrorMessage.style.color = "red";
+  }
+
+  //   if (consentCheck.checked) {
+  //     console.log("Checkbox is checked");
+  //   } else {
+  //     consentCheckErrorMessage.style.color = "red";
+  //   }
+});
